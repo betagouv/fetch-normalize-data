324dfe63c764517d41f0aa15bf474240647eee6f
erwanledoux <lawanledoux@gmail.com>
2019-07-16 10:44:08 +0200

    switched succesData with handleSuccess

--- src/fetch/handleApiSuccess.js
+++ src/fetch/handleApiSuccess.js
@@ -5,11 +5,11 @@ export function handleApiSuccess (reducer, payload, config) {
   const state = { data }
   const { handleSuccess } = config
 
-  dispatch(successData(payload, config))
-
   if (handleSuccess) {
     handleSuccess(state, { config, payload })
   }
+
+  dispatch(successData(payload, config))
 }
 
 export default handleApiSuccess
