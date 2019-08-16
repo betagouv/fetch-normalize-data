f4fbbeaaa6ff13a8ab4526085cbf9c5ee883cedd
Fabien Mercier <fmercier@octo.com>
2019-08-16 17:26:56 +0200

    Update modules for React 16.9

--- precommit.sh
+++ precommit.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # ------
 # ESLint Checking using script
@@ -6,27 +6,26 @@
 # If git is reporting that your prettified files are still modified
 # after committing, you may need to add a post-commit script
 # to update git's index as described in this issue.
-#
-# @see https://prettier.io/docs/en/precommit.html#option-5-bash-script
-#
-jsfiles=$(git diff --cached --name-only --diff-filter=ACM "*.js" "*.jsx" | tr '\n' ' ')
-[ -z "$jsfiles" ] && exit 0
+STAGED_FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E ".js$|.jsx$")
+if [[ "$STAGED_FILES" = "" ]]; then
+    exit 0
+fi
 
-for file in $jsfiles
+for FILE in $STAGED_FILES
 do
-  # we only want to lint the staged changes, not any un-staged changes
-  git show ":$file" | ./node_modules/.bin/eslint --stdin --stdin-filename "$file"
-  if [ $? -ne 0 ]; then
-    echo "ESLint failed on staged file '$file'. Please check your code and try again. You can run ESLint manually via npm run eslint."
-    # exit with failure status
+  ./node_modules/.bin/eslint --quiet --max-warnings 0 "$FILE"
+  if [[ "$?" == 0 ]]; then
+    echo "\t\033[32mESLint Passed: $FILE\033[0m"
+  else
+    echo "\t\033[41mESLint Failed: $FILE\033[0m"
     exit 1
   fi
 done
 
 # Prettify all staged .js files
-echo "$jsfiles" | xargs ./node_modules/.bin/prettier-eslint --eslint-config-path ./.eslintrc.json --config ./.prettierrc.json --list-different --write
+echo "$STAGED_FILES" | xargs ./node_modules/.bin/prettier-eslint --eslint-config-path ./.eslintrc.json --config ./.prettierrc.json --list-different --write
 
 # Add back the modified/prettified files to staging
-echo "$jsfiles" | xargs git add
+echo "$STAGED_FILES" | xargs git add
 
 exit 0
