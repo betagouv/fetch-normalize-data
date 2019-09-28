e4b71d9a186bf9b5423ce23312b56be51103f33e
erwanledoux <lawanledoux@gmail.com>
2019-09-29 00:34:31 +0200

    made null stateKey preventing to add patch to state

--- src/reducer/__specs__/createDataReducer.spec.js
+++ src/reducer/__specs__/createDataReducer.spec.js
@@ -1,40 +1,16 @@
 import { combineReducers, createStore } from 'redux'
 
 import createDataReducer from '../createDataReducer'
-import { assignData, deleteData, reinitializeData } from '../actionCreators'
+import {
+  assignData,
+  deleteData,
+  reinitializeData,
+  successData,
+} from '../actionCreators'
 
 Date.now = jest.spyOn(Date, 'now').mockImplementation(() => 1487076708000)
 
 describe('src | createDataReducer', () => {
-  describe('when RESET_DATA', () => {
-    it('should reset data with no excludes', () => {
-      // given
-      const initialState = { bars: [{ id: 'FF' }], foos: [{ id: 'AE' }], totos: [{ id: 'DD' }]}
-      const rootReducer = combineReducers({ data: createDataReducer(initialState) })
-      const store = createStore(rootReducer)
-
-      // when
-      store.dispatch(reinitializeData())
-
-      // then
-      expect(store.getState().data).toStrictEqual(initialState)
-    })
-
-    it('should reset data with excludes', () => {
-      // given
-      const initialState = { bars: [], foos: [{ id: 'AE' }], totos: [{ id: 'DD1' }]}
-      const rootReducer = combineReducers({ data: createDataReducer(initialState) })
-      const store = createStore(rootReducer)
-      store.dispatch(assignData({ bars: [{ id: 'FF' }], foos: [], totos: [{ id: 'DD1' }, { id: 'DD2' }]}))
-
-      // when
-      store.dispatch(reinitializeData({ excludes: ['bars', 'totos'] }))
-
-      // then
-      expect(store.getState().data).toStrictEqual({ bars: [{ id: 'FF' }], foos: [{ id: 'AE' }], totos: [{ id: 'DD1' }, { id: 'DD2' }]})
-    })
-  })
-
   describe('when ASSIGN_DATA', () => {
     it('should assign data', () => {
       // given
@@ -95,6 +71,106 @@ describe('src | createDataReducer', () => {
         ],
         foos: [],
       })
+    })
+  })
+
+  describe('when REINITIALIZE_DATA', () => {
+    it('should reset data with no excludes', () => {
+      // given
+      const initialState = {
+        bars: [{ id: 'FF' }],
+        foos: [{ id: 'AE' }],
+        totos: [{ id: 'DD' }],
+      }
+      const rootReducer = combineReducers({
+        data: createDataReducer(initialState),
+      })
+      const store = createStore(rootReducer)
+
+      // when
+      store.dispatch(reinitializeData())
+
+      // then
+      expect(store.getState().data).toStrictEqual(initialState)
+    })
+
+    it('should reset data with excludes', () => {
+      // given
+      const initialState = {
+        bars: [],
+        foos: [{ id: 'AE' }],
+        totos: [{ id: 'DD1' }],
+      }
+      const rootReducer = combineReducers({
+        data: createDataReducer(initialState),
+      })
+      const store = createStore(rootReducer)
+      store.dispatch(
+        assignData({
+          bars: [{ id: 'FF' }],
+          foos: [],
+          totos: [{ id: 'DD1' }, { id: 'DD2' }],
+        })
+      )
+
+      // when
+      store.dispatch(reinitializeData({ excludes: ['bars', 'totos'] }))
+
+      // then
+      expect(store.getState().data).toStrictEqual({
+        bars: [{ id: 'FF' }],
+        foos: [{ id: 'AE' }],
+        totos: [{ id: 'DD1' }, { id: 'DD2' }],
+      })
+    })
+  })
+
+  describe('when SUCCESS_DATA', () => {
+    it('should merge a patch inside the state with apiPath', () => {
+      const initialState = { bars: [] }
+      const rootReducer = combineReducers({
+        data: createDataReducer(initialState),
+      })
+      const store = createStore(rootReducer)
+
+      // when
+      const foos = [{ id: 'AE' }]
+      store.dispatch(
+        successData(
+          { data: foos, status: 200 },
+          { apiPath: '/foos', method: 'GET' }
+        )
+      )
+
+      // then
+      const expectedFoos = foos.map(foo => ({
+        ...foo,
+        __ACTIVITIES__: ['/foos'],
+      }))
+      expect(store.getState().data).toStrictEqual({
+        bars: [],
+        foos: expectedFoos,
+      })
+    })
+
+    it('should not merge a patch when stateKey is null', () => {
+      const initialState = { bars: [] }
+      const rootReducer = combineReducers({
+        data: createDataReducer(initialState),
+      })
+      const store = createStore(rootReducer)
+
+      // when
+      const foos = [{ id: 'AE' }]
+      store.dispatch(
+        successData(
+          { data: foos, status: 200 },
+          { apiPath: '/foos', method: 'GET', stateKey: null }
+        )
+      )
+
+      // then
+      expect(store.getState().data).toStrictEqual({ bars: [] })
     })
   })
 })
