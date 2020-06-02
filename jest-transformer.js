487ce92eb5b5942c625e3e4e63c81cf49d26d10b
Eledoux <lawanledoux@gmail.com>
2020-06-02 22:42:59 +0200

    upgraded babel

--- jest-transformer.js
+++ jest-transformer.js
@@ -1,6 +1,6 @@
 const config = {
   babelrc: false,
-  plugins: ['transform-es2015-modules-commonjs'],
+  plugins: ['@babel/transform-modules-commonjs'],
 }
 
 module.exports = require('babel-jest').createTransformer(config)
