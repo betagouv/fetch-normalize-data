b7844c3f4ac147975f350a1728b6b1c08da7b6ee
Eledoux <lawanledoux@gmail.com>
2020-06-04 01:04:52 +0200

    added selectEntitiesByKeyAndActivityTags

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,4 +1,9 @@
+import { default as selectEntitiesByKeyAndActivityTags } from './selectEntitiesByKeyAndActivityTags'
 import { default as selectEntityByKeyAndId } from './selectEntityByKeyAndId'
 import { default as selectEntitiesByKeyAndJoin } from './selectEntitiesByKeyAndJoin'
 
-export { selectEntityByKeyAndId, selectEntitiesByKeyAndJoin }
+export {
+  selectEntitiesByKeyAndActivityTags,
+  selectEntityByKeyAndId,
+  selectEntitiesByKeyAndJoin,
+}
