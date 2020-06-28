5af76c1492ce3bc95bf3436c058e38466a770c3c
Eledoux <lawanledoux@gmail.com>
2020-06-28 03:04:11 +0200

    prepare commit data

--- src/reducers/data/createDataReducer.js
+++ src/reducers/data/createDataReducer.js
@@ -1,5 +1,6 @@
 import {
   ASSIGN_DATA,
+  COMMIT_DATA,
   DELETE_DATA,
   MERGE_DATA,
   REINITIALIZE_DATA,
@@ -8,13 +9,26 @@ import reinitializeState from './reinitializeState'
 import getSuccessState from './getSuccessState'
 import getDeletedPatchByActivityTag from './getDeletedPatchByActivityTag'
 
+import getNormalizedCommitedState from '../../normalize/getNormalizedCommitedState'
 import getNormalizedDeletedState from '../../normalize/getNormalizedDeletedState'
 import getNormalizedMergedState from '../../normalize/getNormalizedMergedState'
 
 export const createDataReducer = (initialState = {}) => {
   const reducer = (state = initialState, action) => {
     if (action.type === ASSIGN_DATA) {
-      return Object.assign({}, state, action.patch)
+      return { ...state, ...action.patch }
+    }
+
+    if (action.type === COMMIT_DATA) {
+      const nextState = getNormalizedMergedState(
+        nextState,
+        { __COMMITS__: action.commits },
+        {
+          getDatumIdKey: () => '__COMMIT_IDENTIFIER__',
+          getDatumIdValue: commit => commit.id || `${commit.uuid}/${commit.dateCreated}`,
+          isMergingDatum: true
+        })
+      return { ...state, ...nextState }
     }
 
     if (action.type === DELETE_DATA) {
@@ -23,7 +37,7 @@ export const createDataReducer = (initialState = {}) => {
         patch = getDeletedPatchByActivityTag(patch, action.config.activityTags)
       }
       const nextState = getNormalizedDeletedState(state, patch, action.config)
-      return Object.assign({}, state, nextState)
+      return { ...state, ...nextState }
     }
 
     if (action.type === MERGE_DATA) {
@@ -32,7 +46,7 @@ export const createDataReducer = (initialState = {}) => {
         action.patch,
         action.config
       )
-      return Object.assign({}, state, nextState)
+      return { ...state, ...nextState }
     }
 
     if (action.type === REINITIALIZE_DATA) {
@@ -41,8 +55,28 @@ export const createDataReducer = (initialState = {}) => {
 
     if (/SUCCESS_DATA_(DELETE|GET|POST|PUT|PATCH)_(.*)/.test(action.type)) {
       const nextState = getSuccessState(state, action)
-      return Object.assign({}, state, nextState)
+      return { ...state, ...nextState }
     }
+
+
+    let nextState = state
+    let needsToUpdateCommitCollectionsBecauseChange
+    if (action.type === 'persist/REHYDRATE') {
+      needsToUpdateCommitCollectionsBecauseChange =
+        typeof action.payload !== 'undefined' &&
+        typeof action.payload.__COMMITS__ !== 'undefined'
+      nextState = action.payload
+    } else {
+      needsToUpdateCommitCollectionsBecauseChange =
+      typeof nextState.commits !== 'undefined' &&
+      state.__COMMITS__ !== nextState.__COMMITS__
+    }
+    if (needsToUpdateCommitCollectionsBecauseChange) {
+      const patch = { __COMMITS__: nextState.__COMMITS__ }
+      const nextState = getNormalizedCommitedState(state, patch, action.config)
+      return { ...state, ...nextState }
+    }
+
 
     return state
   }
