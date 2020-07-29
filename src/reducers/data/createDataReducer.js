6b793fe3134477d7152a7ff00f872fb32262a317
Eledoux <lawanledoux@gmail.com>
2020-07-29 22:04:43 +0200

    better syntax

--- src/reducers/data/createDataReducer.js
+++ src/reducers/data/createDataReducer.js
@@ -16,46 +16,62 @@ import getNormalizedMergedState from '../../normalize/getNormalizedMergedState'
 export const createDataReducer = (initialState = {}) => {
   const reducer = (state = initialState, action) => {
     if (action.type === ASSIGN_DATA) {
-      return { ...state, ...action.patch }
+      return {
+        ...state,
+        ...action.patch
+      }
     }
 
     if (action.type === COMMIT_DATA) {
-      const nextState = getNormalizedMergedState(
-        nextState,
-        { __COMMITS__: action.commits },
-        {
-          getDatumIdKey: () => '__COMMIT_IDENTIFIER__',
-          getDatumIdValue: commit => commit.id || `${commit.uuid}/${commit.dateCreated}`,
-          isMergingDatum: true
-        })
-      return { ...state, ...nextState }
+      /*
+      const mergedState =
+      return {
+        ...state,
+        ...getNormalizedMergedState(
+          nextState,
+          { __COMMITS__: action.commits },
+          {
+            getDatumIdKey: () => '__COMMIT_IDENTIFIER__',
+            getDatumIdValue: commit => commit.id || `${commit.uuid}/${commit.dateCreated}`,
+            isMergingDatum: true
+          }) }
+      */
     }
 
     if (action.type === DELETE_DATA) {
       let patch = action.patch || state
       if (action.config.activityTags) {
-        patch = getDeletedPatchByActivityTag(patch, action.config.activityTags)
+        patch = getDeletedPatchByActivityTag(patch,
+                                             action.config.activityTags)
       }
-      const nextState = getNormalizedDeletedState(state, patch, action.config)
-      return { ...state, ...nextState }
+      return {
+        ...state,
+        ...getNormalizedDeletedState(state,
+                                     patch,
+                                     action.config)
+      }
     }
 
     if (action.type === MERGE_DATA) {
-      const nextState = getNormalizedMergedState(
-        state,
-        action.patch,
-        action.config
-      )
-      return { ...state, ...nextState }
+      return {
+        ...state,
+        ...getNormalizedMergedState(state,
+                                    action.patch,
+                                    action.config)
+      }
     }
 
     if (action.type === REINITIALIZE_DATA) {
-      return reinitializeState(state, initialState, action.config)
+      return reinitializeState(state,
+                               initialState,
+                               action.config)
     }
 
     if (/SUCCESS_DATA_(DELETE|GET|POST|PUT|PATCH)_(.*)/.test(action.type)) {
-      const nextState = getSuccessState(state, action)
-      return { ...state, ...nextState }
+      return {
+        ...state,
+        ...getSuccessState(state, action)
+      }
     }
 
 
