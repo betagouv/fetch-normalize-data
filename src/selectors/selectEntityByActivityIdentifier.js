4a0baf02fa902bb5a11b9eed0c0607b2f8dce92a
Eledoux <lawanledoux@gmail.com>
2020-12-07 12:22:11 +0100

    guard to avoid _persist and __activities__ when brute search via activityIdentifier

--- src/selectors/selectEntityByActivityIdentifier.js
+++ src/selectors/selectEntityByActivityIdentifier.js
@@ -3,29 +3,45 @@ import createCachedSelector from 're-reselect'
 
 import selectEntityByKeyAndJoin from './selectEntityByKeyAndJoin'
 
-const mapArgsToCacheKey = (state, activityIdentifier) => activityIdentifier || ' '
+const mapArgsToCacheKey = (state, activityIdentifier) =>
+  activityIdentifier || ' '
 
 export default createCachedSelector(
   state => state.data,
   (state, activityIdentifier) => activityIdentifier,
   (data, activityIdentifier) => {
     const __activities__ = data.__activities__
-    const firstActivity = selectEntityByKeyAndJoin({ data: { __activities__ } },
-                                                   '__activities__',
-                                                   { key: 'identifier', value: activityIdentifier })
+    const firstActivity = selectEntityByKeyAndJoin(
+      { data: { __activities__ } },
+      '__activities__',
+      { key: 'identifier', value: activityIdentifier }
+    )
     if (firstActivity && firstActivity.modelName) {
-      const key = pluralize(`${firstActivity.modelName[0].toLowerCase()}${firstActivity.modelName.slice(1)}`)
-      return selectEntityByKeyAndJoin({ data: { [key]: data[key] } },
-                                      key,
-                                      { key: 'activityIdentifier', value: activityIdentifier })
+      const key = pluralize(
+        `${firstActivity.modelName[0].toLowerCase()}${firstActivity.modelName.slice(
+          1
+        )}`
+      )
+      return selectEntityByKeyAndJoin({ data: { [key]: data[key] } }, key, {
+        key: 'activityIdentifier',
+        value: activityIdentifier,
+      })
     }
 
     let entityFoundByBruteSearch
     Object.keys(data).find(key => {
-        entityFoundByBruteSearch = selectEntityByKeyAndJoin({ data: { [key]: data[key] } },
-                                                            key,
-                                                            { key: 'activityIdentifier', value: activityIdentifier })
-        return entityFoundByBruteSearch
+      if (
+        key === '_persist' ||
+        key === '__activities__' ||
+        !Array.isArray(data[key])
+      )
+        return
+      entityFoundByBruteSearch = selectEntityByKeyAndJoin(
+        { data: { [key]: data[key] } },
+        key,
+        { key: 'activityIdentifier', value: activityIdentifier }
+      )
+      return entityFoundByBruteSearch
     })
     return entityFoundByBruteSearch
   }
