9c9ae51696338cfe11d7590e91fd85ede3799005
Eledoux <lawanledoux@gmail.com>
2020-12-24 00:51:30 +0100

    merge also with activityIdentifier if exists

--- src/normalize/utils.js
+++ src/normalize/utils.js
@@ -1,11 +1,16 @@
 import pluralize from 'pluralize'
 
-export function getDefaultDatumIdKey() {
-  return 'id'
+export function getDefaultDatumIdKey(datum) {
+  if (typeof datum.id !== 'undefined') return 'id'
+  if (typeof datum.activityIdentifier !== 'undefined')
+    return 'activityIdentifier'
 }
 
 export function getDefaultDatumIdValue(datum, index) {
-  return datum.id || index
+  if (typeof datum.id !== 'undefined') return datum.id
+  if (typeof datum.activityIdentifier !== 'undefined')
+    return datum.activityIdentifier
+  return index
 }
 
 export function getDefaultActivityFrom() {
@@ -18,7 +23,10 @@ export function hydratedActivityFrom(activity) {
     if (activity.tableName) {
       stateKey = pluralize(activity.tableName, 2)
     } else if (activity.modelName) {
-      stateKey = pluralize(`${activity.modelName[0].toLowerCase()}${activity.modelName.slice(1)}`, 2)
+      stateKey = pluralize(
+        `${activity.modelName[0].toLowerCase()}${activity.modelName.slice(1)}`,
+        2
+      )
     } else {
       console.warn(
         'Missing stateKey or tableName or modelName for that activity.'
@@ -36,13 +44,17 @@ export const merge = (target, source) => {
     if (source[key] instanceof Object) {
       if (Array.isArray(source[key])) {
         if (source[key][0]) {
-          if (!(source[key][0] instanceof Object) || Array.isArray(source[key][0])) {
+          if (
+            !(source[key][0] instanceof Object) ||
+            Array.isArray(source[key][0])
+          ) {
             target[key] = source[key]
             continue
           }
         }
         target[key] = source[key].map((s, index) =>
-          merge({...(target[key] && target[key][index])}, s))
+          merge({ ...(target[key] && target[key][index]) }, s)
+        )
       } else {
         target[key] = merge({ ...target[key] }, source[key])
       }
