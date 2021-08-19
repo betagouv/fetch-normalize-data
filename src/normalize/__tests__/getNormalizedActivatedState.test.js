53711958745449c64ede66e88c9481a8653e120d
Eledoux <lawanledoux@gmail.com>
2021-08-19 17:09:20 +0200

    fix tests

--- src/normalize/__tests__/getNormalizedActivatedState.test.js
+++ src/normalize/__tests__/getNormalizedActivatedState.test.js
@@ -5,171 +5,35 @@ describe('src | getNormalizedActivatedState', () => {
     // given
     const state = {}
 
-    const firstDateCreated = new Date().toISOString()
-    let lastDateCreated = new Date(firstDateCreated)
-    lastDateCreated.setDate(lastDateCreated.getDate() + 1)
-    lastDateCreated = lastDateCreated.toISOString()
     const entityIdentifier = 1
-    const patch = {
-      __activities__: [
-        {
-          dateCreated: firstDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            value: 1,
-          },
+    const activities = [
+      {
+        entityIdentifier,
+        modelName: 'Foo',
+        patch: {
+          value: 1,
         },
-        {
-          dateCreated: lastDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            value: 2,
-          },
+        stateKey: 'foos',
+      },
+      {
+        entityIdentifier,
+        modelName: 'Foo',
+        patch: {
+          value: 2,
         },
-      ],
-    }
+        stateKey: 'foos',
+      },
+    ]
 
     // when
-    const nextState = getNormalizedActivatedState(state, patch)
+    const nextState = getNormalizedActivatedState(state, activities)
 
     // then
     expect(nextState).toStrictEqual({
-      __activities__: [
-        {
-          dateCreated: firstDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            value: 1,
-          },
-        },
-        {
-          dateCreated: lastDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            value: 2,
-          },
-        },
-      ],
       foos: [
         {
           activityIdentifier: entityIdentifier,
-          dateCreated: firstDateCreated,
-          dateModified: lastDateCreated,
           value: 2,
-        },
-      ],
-    })
-  })
-
-  it('should delete an entity when met a delete activity', () => {
-    // given
-    const state = {}
-
-    const firstDateCreated = new Date().toISOString()
-    let lastDateCreated = new Date(firstDateCreated)
-    lastDateCreated.setDate(lastDateCreated.getDate() + 1)
-    lastDateCreated = lastDateCreated.toISOString()
-    const entityIdentifier = 1
-    const patch = {
-      __activities__: [
-        {
-          dateCreated: firstDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            value: 1,
-          },
-        },
-        {
-          dateCreated: lastDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {},
-          verb: 'delete',
-        },
-      ],
-    }
-
-    // when
-    const nextState = getNormalizedActivatedState(state, patch)
-
-    // then
-    expect(nextState).toStrictEqual({
-      __activities__: [
-        {
-          dateCreated: firstDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            value: 1,
-          },
-        },
-        {
-          dateCreated: lastDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {},
-          verb: 'delete',
-        },
-      ],
-    })
-  })
-
-  it('should delete an entity when met a patch.isSoftDeleted activity', () => {
-    // given
-    const state = {}
-
-    const firstDateCreated = new Date().toISOString()
-    let lastDateCreated = new Date(firstDateCreated)
-    lastDateCreated.setDate(lastDateCreated.getDate() + 1)
-    lastDateCreated = lastDateCreated.toISOString()
-    const entityIdentifier = 1
-    const patch = {
-      __activities__: [
-        {
-          dateCreated: firstDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            value: 1,
-          },
-        },
-        {
-          dateCreated: lastDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            isSoftDeleted: true,
-          },
-        },
-      ],
-    }
-
-    // when
-    const nextState = getNormalizedActivatedState(state, patch)
-
-    // then
-    expect(nextState).toStrictEqual({
-      __activities__: [
-        {
-          dateCreated: firstDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            value: 1,
-          },
-        },
-        {
-          dateCreated: lastDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            isSoftDeleted: true,
-          },
         },
       ],
     })
@@ -180,46 +44,28 @@ describe('src | getNormalizedActivatedState', () => {
     const entityIdentifier = 1
     const state = {}
 
-    const firstDateCreated = new Date().toISOString()
-    const patch = {
-      __activities__: [
-        {
-          dateCreated: firstDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            positions: [
-              [0.1, 0.2],
-              [0.3, 0.4],
-            ],
-          },
+    const activities = [
+      {
+        entityIdentifier,
+        modelName: 'Foo',
+        patch: {
+          positions: [
+            [0.1, 0.2],
+            [0.3, 0.4],
+          ],
         },
-      ],
-    }
+        stateKey: 'foos',
+      },
+    ]
 
     // when
-    const nextState = getNormalizedActivatedState(state, patch)
+    const nextState = getNormalizedActivatedState(state, activities)
 
     // then
     expect(nextState).toStrictEqual({
-      __activities__: [
-        {
-          dateCreated: firstDateCreated,
-          entityIdentifier,
-          modelName: 'Foo',
-          patch: {
-            positions: [
-              [0.1, 0.2],
-              [0.3, 0.4],
-            ],
-          },
-        },
-      ],
       foos: [
         {
           activityIdentifier: entityIdentifier,
-          dateCreated: firstDateCreated,
-          dateModified: null,
           positions: [
             [0.1, 0.2],
             [0.3, 0.4],
